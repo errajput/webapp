cd5d85b542d43f899f1ad68b6d4a459fecfb0659
errajput <dishacsevira@gmail.com>
2023-12-05 19:41:44 +0530

    add fibonacci series

---
+++ Fibonacci Series/main.js
@@ -0,0 +1,12 @@
+const fibonacci_series = function (n) {
+  if (n == 1) {
+    var loop = [0, 1];
+    document.getElementById("output").innerHTML = loop;
+    return loop;
+  } else {
+    const s = fibonacci_series(n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    document.getElementById("output").innerHTML = s;
+    return s;
+  }
+};
